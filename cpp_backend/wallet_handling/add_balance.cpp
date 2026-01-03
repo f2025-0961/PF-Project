f2515f39875c72d05a2dfd542741bdc400053d44
javeria0990 <f2025-0990@bnu.edu.pk>
2026-01-03 12:55:00 +0500

    add balance function done

---
+++ cpp_backend/wallet_handling/add_balance.cpp
@@ -0,0 +1,69 @@
+#include<iostream>
+#include <fstream>
+#include <sstream>
+#include <vector>
+using namespace std;
+
+int main(int argument, char *arg[])
+{
+    bool pkrBalanceUpdate = false;
+    int userID = stoi(arg[1]);
+    int balanceType = stoi(arg[2]);
+    double balanceToAdd = stod(arg[3]);
+    if (balanceType == 1)
+    {
+        pkrBalanceUpdate = true;
+    }
+    vector<string> linesV;
+    ifstream walletFile;
+    walletFile.open("wallet.txt");
+    if (walletFile.is_open())
+    {
+        string line;
+        while (getline(walletFile, line))
+        {
+            linesV.push_back(line);
+        }
+        for (string &x : linesV)
+        {
+            int uid;
+            char separator;
+            double totalPkrBalance, totalUsdBalance;
+            stringstream ss(x);
+            ss >> uid >> separator >> totalPkrBalance >> separator >> totalUsdBalance;
+            if (uid == userID)
+            {
+                if (pkrBalanceUpdate)
+                {
+                    totalPkrBalance = totalPkrBalance + balanceToAdd;
+                }
+                else
+                {
+                    totalUsdBalance = totalUsdBalance + balanceToAdd;
+                }
+                x = to_string(uid) + "|" + to_string(totalPkrBalance) + "|" + to_string(totalUsdBalance);
+                break;
+            }
+        }
+        walletFile.close();
+        ofstream rewriteWalletFile;
+        rewriteWalletFile.open("wallet.txt", ios::trunc);
+        if (rewriteWalletFile.is_open())
+        {
+            for (string x : linesV)
+            {
+                rewriteWalletFile << x << endl;
+            }
+            rewriteWalletFile.close();
+        }
+        else
+        {
+            return -1;
+        }
+    }
+    else
+    {
+        return -1;
+    }
+    return 0;
+}
